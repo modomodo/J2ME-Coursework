f375e853c3c233cfcc20d8eab2e02bc761a2b4d8
Michael Muita <mugz.mike@gmail.com>
2015-12-01 03:51:00 +0300

    Code Clean-up

--- adminpolice/src/adminpolice.java
+++ adminpolice/src/adminpolice.java
@@ -25,32 +25,34 @@ import javax.microedition.midlet.*;
  * @author RODGEE
  */
 public class adminpolice extends MIDlet implements ActionListener {
+
     Form current, frmLogin, frmMenu, frmRegister, frmUpdatesR;
     TextField txtRegNo, txtRegNoR, txtFirstNameR, txtLastNameR, txtIDNoR;
     TextArea txtaUpdates;
     Button btnLogin, btnRegister, btnSummary, btnDL, btnInsuaranceDetails, btnOffences, btnUpdates, btnRegisterR, btnUpdatesR;
     Command cmdExit, cmdBack;
+
     public void startApp() {
         Display.init(this);
-        try{
+        try {
             //access the theme created in the resource editor
             Resources res = Resources.open("/adminpolice.res");
             UIManager.getInstance().setThemeProps(res.getTheme("adminpolice"));
-        }catch (IOException ex){
+        } catch (IOException ex) {
             System.out.println("Unable to get theme" + ex.getMessage());
         }
-        
+
         frmLogin = new Form("Login");
         cmdExit = new Command("Exit", 0);
         frmLogin.setLayout(new BoxLayout(BoxLayout.Y_AXIS));
         txtRegNo = new TextField("", 20);
-        
+
         btnLogin = new Button("Login");
         btnLogin.addActionListener(new ActionListener() {
 
             public void actionPerformed(ActionEvent ae) {
-                ShowMenu ();
-               
+                ShowMenu();
+
             }
         });
         btnRegister = new Button("Register");
@@ -58,20 +60,20 @@ public class adminpolice extends MIDlet implements ActionListener {
 
             public void actionPerformed(ActionEvent ae) {
                 ShowRegister();
-               
+
             }
         });
         btnUpdatesR = new Button("Updates");
         btnUpdatesR.addActionListener(new ActionListener() {
 
             public void actionPerformed(ActionEvent ae) {
-                ShowUpdatesR ();
-               
+                ShowUpdatesR();
+
             }
         });
         frmLogin.addComponent(new Label("Registration Number"));
         frmLogin.addComponent(txtRegNo);
-        
+
         frmLogin.addComponent(btnLogin);
         frmLogin.addComponent(btnRegister);
         frmLogin.addComponent(btnUpdatesR);
@@ -79,125 +81,130 @@ public class adminpolice extends MIDlet implements ActionListener {
         frmLogin.addCommandListener(this);
         frmLogin.show();
     }
-    public void ShowMenu (){
+
+    public void ShowMenu() {
         frmMenu = new Form("Welcome");
         frmMenu.setLayout(new BoxLayout(BoxLayout.Y_AXIS));
         btnSummary = new Button("Summary");
         btnSummary.addActionListener(new ActionListener() {
 
-             public void actionPerformed(ActionEvent ae) {
-                 ShowSummary();
-             }
-         });
+            public void actionPerformed(ActionEvent ae) {
+                ShowSummary();
+            }
+        });
         btnDL = new Button("Driving License");
         btnDL.addActionListener(new ActionListener() {
 
-             public void actionPerformed(ActionEvent ae) {
-                 ShowDL();
-             }
-         });
-         btnInsuaranceDetails = new Button("Insuarance Details");
-         btnInsuaranceDetails.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ae) {
+                ShowDL();
+            }
+        });
+        btnInsuaranceDetails = new Button("Insuarance Details");
+        btnInsuaranceDetails.addActionListener(new ActionListener() {
 
-             public void actionPerformed(ActionEvent ae) {
-                 ShowInsuaranceDetails ();
-             }
-         });
-         btnOffences = new Button("Offences");
-         btnOffences.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ae) {
+                ShowInsuaranceDetails();
+            }
+        });
+        btnOffences = new Button("Offences");
+        btnOffences.addActionListener(new ActionListener() {
 
-             public void actionPerformed(ActionEvent ae) {
-                ShowOffences ();
-             }
-         });
-         btnUpdates = new Button("Updates");
-         btnUpdates.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ae) {
+                ShowOffences();
+            }
+        });
+        btnUpdates = new Button("Updates");
+        btnUpdates.addActionListener(new ActionListener() {
 
-             public void actionPerformed(ActionEvent ae) {
-                ShowUpdates ();
-             }
-         });
-         frmMenu.addComponent(btnSummary);
-         frmMenu.addComponent(btnDL);
-         frmMenu.addComponent(btnInsuaranceDetails);
-         frmMenu.addComponent(btnOffences);
-         frmMenu.addComponent(btnUpdates);
-         cmdBack = new Command("Back", 0);
-         frmMenu.addCommand(cmdBack);
-         frmMenu.addCommandListener(this);
-         frmMenu.show();
+            public void actionPerformed(ActionEvent ae) {
+                ShowUpdates();
+            }
+        });
+        frmMenu.addComponent(btnSummary);
+        frmMenu.addComponent(btnDL);
+        frmMenu.addComponent(btnInsuaranceDetails);
+        frmMenu.addComponent(btnOffences);
+        frmMenu.addComponent(btnUpdates);
+        cmdBack = new Command("Back", 0);
+        frmMenu.addCommand(cmdBack);
+        frmMenu.addCommandListener(this);
+        frmMenu.show();
     }
-     private void ShowRegister(){
-         frmRegister = new Form("Register");
-         frmRegister.setLayout(new BorderLayout());
-         txtRegNoR = new TextField("", 20);
-         txtFirstNameR = new TextField("", 20);
-         txtLastNameR = new TextField("", 20);
-         btnRegisterR = new Button("Register");
-         btnRegisterR.addActionListener(new ActionListener() {
 
-             public void actionPerformed(ActionEvent ae) {
-                 frmLogin.show();
-             }
-         });
-         Container conRegister = new Container(new BoxLayout(BoxLayout.Y_AXIS));
-         conRegister.addComponent(new Label("Registration Number"));
-         conRegister.addComponent(txtRegNoR);
-         conRegister.addComponent(new Label("First Name"));
-         conRegister.addComponent(txtFirstNameR);
-         conRegister.addComponent(new Label("Last Name"));
-         conRegister.addComponent(txtLastNameR);
-         conRegister.addComponent(btnRegisterR);
-         //add the container to the form
-         frmRegister.addComponent(BorderLayout.CENTER, conRegister);
-         cmdBack = new Command("Back", 0);
-         frmRegister.addCommand(cmdBack);
-         frmRegister.addCommandListener(this);
-         frmRegister.show();
-         
-     }
-     private void ShowUpdatesR (){
+    private void ShowRegister() {
+        frmRegister = new Form("Register");
+        frmRegister.setLayout(new BorderLayout());
+        txtRegNoR = new TextField("", 20);
+        txtFirstNameR = new TextField("", 20);
+        txtLastNameR = new TextField("", 20);
+        btnRegisterR = new Button("Register");
+        btnRegisterR.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent ae) {
+                frmLogin.show();
+            }
+        });
+        Container conRegister = new Container(new BoxLayout(BoxLayout.Y_AXIS));
+        conRegister.addComponent(new Label("Registration Number"));
+        conRegister.addComponent(txtRegNoR);
+        conRegister.addComponent(new Label("First Name"));
+        conRegister.addComponent(txtFirstNameR);
+        conRegister.addComponent(new Label("Last Name"));
+        conRegister.addComponent(txtLastNameR);
+        conRegister.addComponent(btnRegisterR);
+        //add the container to the form
+        frmRegister.addComponent(BorderLayout.CENTER, conRegister);
+        cmdBack = new Command("Back", 0);
+        frmRegister.addCommand(cmdBack);
+        frmRegister.addCommandListener(this);
+        frmRegister.show();
+
+    }
+
+    private void ShowUpdatesR() {
         frmUpdatesR = new Form("Register Updates");
         frmUpdatesR.setLayout(new BorderLayout());
         //txtaUpdates = new TextArea("", );
-     }
-    
-     private void ShowSummary(){
-         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-     }
-     
-     private void ShowDL(){
-         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-     }
-     
-     private void ShowInsuaranceDetails(){
-         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-     }
-     private void ShowOffences(){
-          throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-     }
-     private void ShowUpdates(){
-          throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-     }
-    
+    }
+
+    private void ShowSummary() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void ShowDL() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void ShowInsuaranceDetails() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void ShowOffences() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void ShowUpdates() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
     public void pauseApp() {
     }
-    
+
     public void destroyApp(boolean unconditional) {
     }
 
     public void actionPerformed(ActionEvent ae) {
         current = Display.getInstance().getCurrent();
-        if(ae.getSource() == cmdExit){
+        if (ae.getSource() == cmdExit) {
             destroyApp(true);
             notifyDestroyed();
-        }else if(ae.getSource() == btnLogin && current == frmLogin){
+        } else if (ae.getSource() == btnLogin && current == frmLogin) {
             frmLogin.show();
-        }else if(ae.getSource() == btnRegister && current == frmLogin){
+        } else if (ae.getSource() == btnRegister && current == frmLogin) {
             frmRegister.show();
-        }else if(ae.getSource() == cmdBack && current == frmMenu){
+        } else if (ae.getSource() == cmdBack && current == frmMenu) {
             frmLogin.show();
-        }else if(ae.getSource() == cmdBack && current == frmRegister){
+        } else if (ae.getSource() == cmdBack && current == frmRegister) {
             frmLogin.show();
         }
     }
